9dfe3617b8228949397d983829a213030f20e86d
desyha <deasyahbila16@gmail.com>
2024-06-20 11:06:38 +0700

    added token log

--- OBENG/api/middleware/verifyToken.js
+++ OBENG/api/middleware/verifyToken.js
@@ -4,6 +4,8 @@ export const verifyToken= (req, res, next) => {
     const token = req.cookies.token
 
     if(!token) return res.status(401).json({message: "not Authenticated"})
+        console.log('Token:', token);
+    console.log('JWT_SECRET_KEY:', process.env.JWT_SECRET_KEY);
         jwt.verify(token, process.env.JWT_SECRET_KEY, async(err, payload)=>{
             if(err) return res.status(403).json({message: "token not valid"})
             req.userId = payload.id
