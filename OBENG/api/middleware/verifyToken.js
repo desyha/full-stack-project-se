ff3cf25e5cd9b833045667218924094f80649b95
desyha <deasyahbila16@gmail.com>
2024-06-20 11:03:07 +0700

    reverted

--- OBENG/api/middleware/verifyToken.js
+++ OBENG/api/middleware/verifyToken.js
@@ -1,16 +1,13 @@
-import jwt from "jsonwebtoken";
+import jwt from "jsonwebtoken"
 
-const verifyToken = (req, res, next) => {
-    const token = req.cookies.token;
-    if (!token) return res.status(401).json({ message: "not Authenticated" });
+export const verifyToken= (req, res, next) => {
+    const token = req.cookies.token
 
-    console.log('JWT_SECRET_KEY:', process.env.JWT_SECRET_KEY); // Log the secret key to ensure it is set
-
-    jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
-        if (err) return res.status(403).json({ message: "token not valid" });
-        req.userId = payload.id;
-        next();
-    });
-};
-
-export default verifyToken;
+    if(!token) return res.status(401).json({message: "not Authenticated"})
+        jwt.verify(token, process.env.JWT_SECRET_KEY, async(err, payload)=>{
+            if(err) return res.status(403).json({message: "token not valid"})
+            req.userId = payload.id
+            next()
+            }    
+        )
+}
